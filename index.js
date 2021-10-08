c35944e66702efc337ba903673d92938e30999c3
Beau Hibbert <beauhibbert789@gmail.com>
2021-10-07 23:06:59 -0700

    move button variable just above event listener

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
-let button = document.getElementById('submitButton');
 let pTag = document.getElementById('output');
 let pTagContent = '';
 
 function determineFavoriteTeam() {
   let userInput = document.getElementById('userInput').value.toLowerCase();
+
+
   if (userInput === 'washington state') {
     pTagContent = 'Go Cougs!';
   } else {
@@ -16,6 +17,7 @@ function determineFavoriteTeam() {
   console.log(pTagContent);
 }
 
+let button = document.getElementById('submitButton');
 button.addEventListener('click', determineFavoriteTeam);
 
 
