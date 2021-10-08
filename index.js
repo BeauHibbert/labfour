8a6cfc870164f1dc2cb5a8ff64f858a2e64102e8
Beau Hibbert <beauhibbert789@gmail.com>
2021-10-07 22:58:01 -0700

    move userInput variable inside determineFavoriteTeam function

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
-let userInput = document.getElementById('userInput').value.toLowerCase();
-console.log(userInput);
 let button = document.getElementById('submitButton');
 let pTag = document.getElementById('output');
 let pTagContent = '';
 
 function determineFavoriteTeam() {
+  let userInput = document.getElementById('userInput').value.toLowerCase();
   if (userInput === 'washington state') {
     pTagContent = 'Go Cougs!';
   } else {
