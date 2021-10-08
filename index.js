27608cc2a8066bc87e1ed5be7afdabb7cfb44806
Beau Hibbert <beauhibbert789@gmail.com>
2021-10-07 22:50:13 -0700

    fix toLowerCase method capitalization

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-let userInput = document.getElementById("userInput").value.toLowercase();
+let userInput = document.getElementById("userInput").value.toLowerCase();
+console.log(userInput);
 let button = document.getElementById("submitButton");
 let pTag = document.getElementById("output");
 let pTagContent = '';
